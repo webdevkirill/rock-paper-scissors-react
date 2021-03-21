acffec5bfd9974910620bf82f5b4f16e46ed89b5
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-21 15:10:02 +0300

    Добавляет верстку для игры

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+	return (
+		<div className='app'>
+			<div className='info'>
+				<h2>Камень-ножницы-бумага</h2>
+				<div className='wins-losses'>
+					<div className='wins'>
+						<span className='number'>0</span>
+						<span className='text'>Побед</span>
+					</div>
 
-export default App;
+					<div className='losses'>
+						<span className='number'>0</span>
+						<span className='text'>Поражений</span>
+					</div>
+				</div>
+			</div>
+
+			<div className='choices'>
+				<div>Вы</div>
+				<div />
+				<div>Компьютер</div>
+
+				<div>
+					<button className='rock'>Комень</button>
+					<button className='paper'>Бумага</button>
+					<button className='scissors'>Ножницы</button>
+				</div>
+
+				<div className='vs'>Против</div>
+
+				<div>
+					<button className='computer-choice'>?</button>
+				</div>
+			</div>
+		</div>
+	);
+}
