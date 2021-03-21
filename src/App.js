f5eadaf52fed27ac2ad0294bcc61c4258923c12b
webdevkirill <kirill-fjodorov000@yandex.ru>
2021-03-21 15:57:35 +0300

    Добавляет вывод модального окна при совершении выбора

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import Rock from './icons/Rock';
-import Paper from './icons/Paper';
-import Scissors from './icons/Scissors';
+import { Rock } from './icons/Rock';
+import { Paper } from './icons/Paper';
+import { Scissors } from './icons/Scissors';
 
 const choices = [
 	{ id: 1, name: 'rock', component: Rock },
@@ -13,6 +13,9 @@ const choices = [
 export default function App() {
 	const [userChose, setUserChose] = useState(null);
 	const [computerChose, setComputerChose] = useState(null);
+	const [wins, setWins] = useState(0);
+	const [losses, setLosses] = useState(0);
+	const [gameState, setGameState] = useState(null);
 
 	useEffect(() => {
 		const randomChoice =
@@ -22,6 +25,12 @@ export default function App() {
 
 	const userChoiseHandler = (choice) => {
 		setUserChose(choice);
+		setGameState('win');
+	};
+
+	const renderComponent = (choice) => {
+		const Component = choice.component;
+		return <Component />;
 	};
 
 	return (
@@ -30,16 +39,28 @@ export default function App() {
 				<h2>Камень-ножницы-бумага</h2>
 				<div className='wins-losses'>
 					<div className='wins'>
-						<span className='number'>0</span>
+						<span className='number'>{wins}</span>
 						<span className='text'>Побед</span>
 					</div>
 
 					<div className='losses'>
-						<span className='number'>0</span>
+						<span className='number'>{losses}</span>
 						<span className='text'>Поражений</span>
 					</div>
 				</div>
 			</div>
+
+			{gameState && (
+				<div className={`game-state ${gameState}`}>
+					<div>
+						<div className='game-state-content'>
+							<p>{renderComponent(userChose)}</p>
+							<p>Вы победили!</p>
+							<p>{renderComponent(computerChose)}</p>
+						</div>
+					</div>
+				</div>
+			)}
 
 			<div className='choices'>
 				<div>Вы</div>
@@ -53,7 +74,7 @@ export default function App() {
 							className={choice.name}
 							onClick={() => userChoiseHandler(choice)}
 						>
-							<choice.component />
+							{renderComponent(choice)}
 						</button>
 					))}
 				</div>
--- src/icons/Rock.js
+++ src/icons/Rock.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function () {
+export const Rock = () => {
 	return (
 		<svg
 			xmlns='http://www.w3.org/2000/svg'
@@ -13,4 +13,4 @@ export default function () {
 			</g>
 		</svg>
 	);
-}
+};
